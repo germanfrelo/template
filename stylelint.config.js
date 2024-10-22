80f987183a99aae6b59c13ccbd9ec36e7c0214ee
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-10-22 08:46:22 +0200

    Add more useful Stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,7 @@
 /** @type {import('stylelint').Config} */
 export default {
 	extends: [
-		// Order matters: later configs take precedence over (override) earlier ones
+		// Order matters: later configs take precedence over (override) earlier ones.
 		"stylelint-config-standard",
 		"stylelint-config-recess-order",
 	],
@@ -10,22 +10,38 @@ export default {
 	reportInvalidScopeDisables: true,
 	reportNeedlessDisables: true,
 	rules: {
-		// Avoid errors: Unknown
+		/**
+		 * ----------------------------------------
+		 * Avoid errors
+		 * ----------------------------------------
+		 */
+
+		// Descending
+		"no-descending-specificity": [true, { severity: "warning" }],
+
+		// Duplicate
+		"font-family-no-duplicate-names": [
+			true,
+			{ ignoreFontFamilyNames: ["monospace"] }, // See https://github.com/search?q=repo:germanfrelo/base-css-stylesheet "monospace, monospace"
+		],
+
+		// Unknown
 		"declaration-property-value-no-unknown": true,
 		"media-feature-name-value-no-unknown": true,
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
 		"no-unknown-custom-properties": true,
 
-		// Enforce conventions: Allowed, disallowed & required
-		"color-named": [
-			"never",
-			{
-				ignore: ["inside-function"],
-			},
-		],
+		/**
+		 * ----------------------------------------
+		 * Enforce conventions
+		 * ----------------------------------------
+		 */
 
-		// Enforce conventions: Max & Min
+		// Empty lines
+		"declaration-empty-line-before": "never",
+
+		// Max & Min
 		"max-nesting-depth": [
 			3,
 			{
@@ -88,14 +104,25 @@ export default {
 			},
 		],
 
-		// Enforce conventions: Patterns
-		// "custom-media-pattern": null,
-		// "custom-property-pattern": null,
-		// "keyframes-name-pattern": null,
-		// "selector-class-pattern": null,
-		// "selector-id-pattern": null,
+		// Notation
+		"media-feature-range-notation": null, // TODO: Remove when browser support is ~96% (https://caniuse.com/css-media-range-syntax)
 
-		// plugin/use-defensive-css
+		// Pattern
+		"custom-media-pattern": null,
+		"custom-property-pattern": null,
+		"keyframes-name-pattern": null,
+		"selector-class-pattern": null,
+		"selector-id-pattern": null,
+
+		// Redundant
+		"declaration-block-no-redundant-longhand-properties": null,
+
+		/**
+		 * ----------------------------------------
+		 * Plugin: use-defensive-css
+		 * ----------------------------------------
+		 */
+
 		"plugin/use-defensive-css": [
 			true,
 			{
