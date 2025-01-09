55417995d24f4809acd172cde0cd7c9069097856
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-01-09 20:39:53 +0100

    Stylelint: Report disable comments that aren't scoped to rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -9,6 +9,7 @@ export default {
 	reportDescriptionlessDisables: true,
 	reportInvalidScopeDisables: true,
 	reportNeedlessDisables: true,
+	reportUnscopedDisables: true,
 	rules: {
 		/**
 		 * ----------------------------------------
