845c4f61288f980caf032084f9b3cd8a75c4908e
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-01-17 10:30:05 +0100

    feat: remove some stylelint rules already added to stylelint-config-recommended in v15.0.0
    References:
    
    - https://github.com/stylelint/stylelint-config-recommended/commit/94cf8b60eb12ee13b8eb308579541c373e6899c6

--- stylelint.config.js
+++ stylelint.config.js
@@ -32,8 +32,6 @@ export default {
 		],
 
 		// Unknown
-		"declaration-property-value-no-unknown": true,
-		"media-feature-name-value-no-unknown": true,
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
 		"no-unknown-custom-properties": true,
