d17a0ebaf146f2aad6c6582c77683d2679bf32b9
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-01-17 16:16:37 +0100

    style: reduce whitespace

--- stylelint.config.js
+++ stylelint.config.js
@@ -11,18 +11,10 @@ export default {
 	reportNeedlessDisables: true,
 	reportUnscopedDisables: true,
 	rules: {
-		/* ----------------------------------------
-		Avoid errors
+		/* Avoid errors
 		---------------------------------------- */
-
 		// Descending
-		"no-descending-specificity": [
-			true,
-			{
-				severity: "warning",
-			},
-		],
-
+		"no-descending-specificity": [true, { severity: "warning" }],
 		// Duplicate
 		"font-family-no-duplicate-names": [
 			true,
@@ -30,36 +22,28 @@ export default {
 				ignoreFontFamilyNames: ["monospace"], // See https://github.com/search?q=repo%3Agermanfrelo%2Fbase-css-stylesheet+%22monospace%2C+monospace%22&type=code
 			},
 		],
-
 		// Unknown
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
 		"no-unknown-custom-properties": true,
 
-		/* ----------------------------------------
-		Enforce conventions
+		/* Enforce conventions
 		---------------------------------------- */
-
 		// Empty lines
 		"declaration-empty-line-before": "never",
-
 		// Notation
 		"media-feature-range-notation": null, // TODO: Remove when browser support is ~96% (https://caniuse.com/css-media-range-syntax)
-
 		// Pattern
 		"custom-media-pattern": null,
 		"custom-property-pattern": null,
 		"keyframes-name-pattern": null,
 		"selector-class-pattern": null,
 		"selector-id-pattern": null,
-
 		// Redundant
 		"declaration-block-no-redundant-longhand-properties": null,
 
-		/* ----------------------------------------
-		Plugin: use-defensive-css
+		/* Plugin: use-defensive-css
 		---------------------------------------- */
-
 		"plugin/use-defensive-css": [
 			true,
 			{
